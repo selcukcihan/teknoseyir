79a1e32d53676b76f9a35ee834e51ecb324f3aa9
selcukcihan <selcukcihan@gmail.com>
2016-03-08 20:57:54 +0200

    stuff

--- app/src/main/java/com/selcukcihan/android/teknoseyir/DeveloperKey.java
+++ app/src/main/java/com/selcukcihan/android/teknoseyir/DeveloperKey.java
@@ -4,5 +4,5 @@ package com.selcukcihan.android.teknoseyir;
  * Created by SELCUKCI on 1.3.2016.
  */
 public class DeveloperKey {
-    public static final String DEVELOPER_KEY = "AIzaSyBFZnYC4vPXjbothf-DZGjE8E_J3pIHNjo";
+    public static final String DEVELOPER_KEY = "";
 }
